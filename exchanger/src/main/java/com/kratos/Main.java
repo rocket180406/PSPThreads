a9166258248c5d4707919b55277f2ff4748b915c
rocket180406 <rocketdans18@gmail.com>
2025-12-01 13:45:18 +0100

    exchanger iniciado

---
+++ exchanger/src/main/java/com/kratos/Main.java
@@ -0,0 +1,9 @@
+package com.kratos;
+/*Crea un programa con 2 hilos, uno de los hilos mandará el literal "Hola!" al otro hilo que a su vez le enviará "Adios".
+
+NOTA: Recuerda que la ejecución se para hasta que todas las partes envían su contenido. */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
